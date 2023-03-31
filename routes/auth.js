ea1fb9b75e8367dad0e7f34fa8763d543b6b8fde
123MayankSharma <mayanksharma922001@gmail.com>
2023-04-01 00:59:41 +0530

    .

--- routes/auth.js
+++ routes/auth.js
@@ -1,64 +1,76 @@
-const express = require("express")
-const router = express.Router()
-const bcrypt=require("bcrypt")
-const User=require("../models/user")
-const jwt=require("jsonwebtoken")
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
-
-
-router.post("/register",async(req,res)=>{
-    try{
-        if(!req.body.password||!req.body.username||!req.body.email){
-            res.status(400).send("one of the input fields is missing...")
-        }else{
-        //generate hashed password
-        const hashedPassword=await bcrypt.hash(req.body.password,10)
-        const newUser=new User({
-            username:req.body.username,
-            email:req.body.email,
-            password:hashedPassword
-        })
-        const user=await newUser.save()
-        res.status(200).json(user)
-      }
-    }catch(err){
-        console.log(err)
-        res.status(500).send(err)
+router.post("/register", async (req, res) => {
+  try {
+    if (!req.body.password || !req.body.username || !req.body.email) {
+      res.status(400).send("one of the input fields is missing...");
+    } else {
+      //generate hashed password
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+      const user = await newUser.save();
+      res.status(200).json(user);
     }
-})
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
-router.post("/",async (req,res)=>{
-    try{
-     if(!req.body.password||!req.body.email){
-            return res.status(400).send("one of the input fields is missing...")
-        }else{
-          const user=await User.findOne({email:req.body.email})
+router.post("/", async (req, res) => {
+  try {
+    if (!req.body.password || !req.body.email) {
+      return res.status(400).send("one of the input fields is missing...");
+    } else {
+      const user = await User.findOne({ email: req.body.email });
 
-        //if user is not found
-        if(!user) return res.status(400).json({"Error":"Entered email or Password is Incorrect!"})
+      //if user is not found
+      if (!user)
+        return res
+          .status(400)
+          .json({ Error: "Entered email or Password is Incorrect!" });
 
-        //then, validate the password
-        const validatePassword=await bcrypt.compare(req.body.password,user.password)
+      //then, validate the password
+      const validatePassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
 
-        //if password of user stored in db does not match password entered by client return client error
+      //if password of user stored in db does not match password entered by client return client error
 
-      if(validatePassword)
-      {
-          jwt.sign({user},process.env.SECRET_KEY,{expiresIn:'11000s'},(err,token)=>{
-            if(err){
-                return res.status(500).json({"error":"backend error..."})
+      if (validatePassword) {
+        jwt.sign(
+          {
+            name: user.username,
+            following: user.following /*this object contains payload*/,
+          },
+          process.env.SECRET_KEY,
+          { expiresIn: "11000s" },
+          (err, token) => {
+            if (err) {
+              return res.status(500).json({ error: "backend error..." });
             }
-            return res.status(200).json({token})
-        })
-      }else{
-          return res.status(400).json({"Error":"Email or Password is Incorrect"})
+            return res.status(200).json({ token });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ Error: "Email or Password is Incorrect" });
       }
     }
-
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 /*
  *
@@ -68,9 +80,6 @@ router.post("/",async (req,res)=>{
  * protected resources.
  *
  *
-*/
+ */
 
-
-
-
-module.exports=router
+module.exports = router;
